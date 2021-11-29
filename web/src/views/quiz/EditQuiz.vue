ac148dd7e9834f8f274e81845ad9f08b22e0a198
João Henrique <h.joaolopes@gmail.com>
2021-11-28 23:56:16 -0300

    Edit

--- web/src/views/quiz/EditQuiz.vue
+++ web/src/views/quiz/EditQuiz.vue
@@ -1,12 +1,12 @@
 <template>
   <h4>{{ $router.currentRoute.value.params['id'] }}</h4>
-  <a-form >
+  <a-form>
 
-    {{quiz}}
+    {{ quiz }}
     <br/>
-    {{alternative}}
+    {{ alternative }}
     <br/>
-    {{questions}}
+    {{ question }}
 
     <a-form-item label="Título">
       <a-input v-model:value="quiz.title"/>
@@ -14,14 +14,33 @@
 
     <a-divider/>
 
+
+    <div v-for="(question, index) in questions" :key="question.id">
+
+      <a-form-item v-bind:label="'Questão ' + (index + 1)">
+        <div style="display: flex">
+          <a-input v-model:value="question.title"/>
+
+          <a-button style="margin-left: 20px" type="secondary" @click.prevent="editQuestion(question)">
+            <EditOutlined/>
+          </a-button>
+          <a-button style="margin-left: 20px" type="primary" danger @click.prevent="">
+            <DeleteOutlined/>
+          </a-button>
+        </div>
+      </a-form-item>
+
+    </div>
+
+
     <a-form-item :wrapper-col="{ span: 5, offset: 4 }" v-if="!creatingNewQuestion">
       <a-button type="primary" @click.prevent="newQuestion()">Adicionar Questões</a-button>
     </a-form-item>
 
     <div v-if="creatingNewQuestion">
 
-      <a-form-item label="Questão 1">
-        <a-input v-model:value="questions.content"/>
+      <a-form-item v-bind:label="'Questão ' + (questions.length+1) ">
+        <a-input v-model:value="question.content"/>
       </a-form-item>
 
 
@@ -29,9 +48,9 @@
 
         <div style="display: flex">
           <a-radio v-model:checked="alternative.correct" @click="checkCorrect(alternative.key)"/>
-          <a-input v-model:value="alternative.description"/>
+          <a-input v-model:value="alternative.content"/>
           <a-button style="margin-left: 20px" type="primary" danger @click.prevent="removeAlternative(alternative.key)">
-            -
+            <DeleteOutlined/>
           </a-button>
         </div>
       </a-form-item>
@@ -55,19 +74,28 @@
 
 import {defineComponent} from 'vue';
 import {baseUrl} from "@/services/QuizService";
+import {
+  EditOutlined,
+  DeleteOutlined
+} from '@ant-design/icons-vue';
 import axios from "axios";
 
 
 export default defineComponent({
+  components: {
+    EditOutlined,
+    DeleteOutlined
+  },
   data() {
     return {
       quiz: {
         id: '',
         title: ''
       },
-      questions: {
+      question: {
         content: '',
       },
+      questions: [],
       alternative: [],
       count: 0,
       creatingNewQuestion: false
@@ -75,6 +103,7 @@ export default defineComponent({
   },
   beforeMount() {
     this.loadQuiz();
+    this.loadQuestions();
   },
   methods: {
     loadQuiz() {
@@ -89,10 +118,46 @@ export default defineComponent({
     onSubmit() {
       if (this.creatingNewQuestion) {
         this.creatingNewQuestion = false;
-        // save question
+        let quizId = this.$router.currentRoute.value.params['id'];
+        axios.post(baseUrl + "/quizzes/" + quizId + "/questions", {
+          question: {
+            quiz_id: quizId,
+            title: this.question.content,
+            alternatives: this.alternative
+          }
+        })
+            .then(res => {
+              console.log(res.data.data);
+              this.question = {
+                content: '',
+              };
+              this.alternative = [];
+              this.loadQuestions();
+            }).catch(err => {
+          console.error(err);
+        })
       } else {
         // save quiz
       }
+    },
+    loadQuestions() {
+      let quizId = this.$router.currentRoute.value.params['id'];
+      axios.get(baseUrl + "/quizzes/" + quizId + "/questions")
+          .then(res => {
+            this.questions = res.data.data;
+          }).catch(err => {
+        console.error(err);
+      })
+    },
+    editQuestion(question) {
+      this.question = question;
+      let quizId = this.$router.currentRoute.value.params['id'];
+      axios.get(baseUrl + "/quizzes/" + quizId + "/questions/" + question.id)
+          .then(res => {
+            this.questions = res.data.data;
+          }).catch(err => {
+        console.error(err);
+      })
     },
     newQuestion() {
       this.creatingNewQuestion = true;
@@ -105,7 +170,7 @@ export default defineComponent({
     },
     newAlternative() {
       this.alternative.push({
-        description: '',
+        content: '',
         correct: false,
         key: this.count++
       })
