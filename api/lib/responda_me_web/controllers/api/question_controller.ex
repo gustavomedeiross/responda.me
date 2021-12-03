c4db9d17092dc07c788d53732ce0538ca6002f39
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2021-12-03 15:31:06 -0300

    Fix question create route

--- api/lib/responda_me_web/controllers/api/question_controller.ex
+++ api/lib/responda_me_web/controllers/api/question_controller.ex
@@ -15,7 +15,7 @@ defmodule Responda.MeWeb.Api.QuestionController do
     with {:ok, %Question{} = question} <- Questions.create_question(question_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.api_question_path(conn, :show, question))
+      |> put_resp_header("location", Routes.api_question_path(conn, :show, question.quiz_id, question))
       |> render("show.json", question: question)
     end
   end
